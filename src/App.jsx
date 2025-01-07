68f1b6214ec2d4b687bd335d1aac98b17fede92a
Johan Cederblad <johanzcederblad@hotmail.com>
2025-01-07 13:11:55 +0100

    npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import {Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
 import Contact from './pages/Contact';
